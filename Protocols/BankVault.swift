e64aacd3fd29108053ecfc24d36728c45c4b43a1
JJJV <juan@jaramillo.net>
2017-01-14 16:28:04 -0600

    Lab solution

--- Protocols/BankVault.swift
+++ Protocols/BankVault.swift
@@ -7,7 +7,7 @@
 //
 
 
-class BankVault {
+class BankVault: ProvideAccess {
     
     let name: String
     let address: String
@@ -18,9 +18,39 @@ class BankVault {
         self.address = address
     }
     
+   
+
+
 }
 
+protocol ProvideAccess {
+    func allowEntryWithPassword (_: [Int]) -> Bool
+}
 
-
-
-
+ extension ProvideAccess {
+        
+    func allowEntryWithPassword (_ password: [Int]) -> Bool {
+    
+        
+        if password.isEmpty {
+            return false
+        } else if password.count > 10 {
+            return false
+        } else {
+            var badCount = 0
+            
+            for (index, i) in password .enumerated() {
+                if  index % 2 == 0 {
+                    if i % 2 != 0 {
+                    badCount += 1
+                    }
+                }
+            }
+            if badCount > 0 {
+                return false
+            } else {
+                return true
+            }
+        }
+        }
+}
